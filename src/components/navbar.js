2ac045496f146be4688e710f93fa7ed48ce6a07d
Davia DeNisco <davia622@gmail.com>
2018-03-11 21:45:20 -0400

    try resolve for packge issue preventing github deployment

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,9 @@ import ttt from '../images/ttt.jpg';
 const NavBar = () =>
 <div id="nav">
   <span>
+    <div id="backSection">
     <a id="back" href="http://www.daviadenisco.com/"> ← </a>
+  </div>
     <img id="logo" src={ttt} className="logo" alt="ttt" />
     <p id="right"></p>
   </span>
